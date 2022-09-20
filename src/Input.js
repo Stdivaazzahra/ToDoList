44a2b8e055c180de7d13d9848bc920a894264f62
Stdivaazzahra <divaazzahra623@gmail.com>
2022-09-20 22:03:46 +0800

    Add File

---
+++ src/Input.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./Input.css";
+import "./ToDo.css";
+import { FaBook } from "react-icons/fa";
+
+const Input = (props) => {
+  const [todo, setTodo] = useState({
+    id: "id-diva~" + new Date().getTime(),
+    task: "",
+    complete: false,
+  });
+  const [todoUpdate, setTodoUpdate] = useState({
+    id: props.update.data.id,
+    task: props.update.data.task,
+    complete: props.update.data.complete,
+  });
+
+  const sumbitBtn = () => {
+    console.log(todoUpdate);
+    if (props.update.update) {
+      props.sub(false);
+      props.inputUpdate(todoUpdate);
+    } else {
+      props.sub(false);
+      props.funcInput(todo);
+    }
+  };
+  console.log(todoUpdate.task);
+
+  return (
+    <div className="input">
+      <h1>{!props.update.update ? "TodoInput" : "Edit Todo"}</h1>
+      <div className="input_wrap">
+        <div className="todo_search">
+          <div className="icon-box">
+            <FaBook className="icon" />
+          </div>
+          <input
+            onChange={(e) =>
+              props.update.update
+                ? setTodoUpdate({ ...todoUpdate, task: e.target.value })
+                : setTodo({ ...todo, task: e.target.value })
+            }
+            type="text"
+            placeholder="Search Todo"
+            value={props.update.update ? todoUpdate.task : todo.task}
+          />
+        </div>
+        <button onClick={sumbitBtn} className="btn__input" type="submit">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Input;
---
+++ src/ToDo.js
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+import Input from "./Input";
+import { FaSearch } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { FaPencilAlt } from "react-icons/fa";
+import list from "./List.json";
+import "./ToDo.css";
+const ToDo = () => {
+  const [openInput, setOpenInput] = useState(false);
+  const [todos, setTodos] = useState(list);
+  const [dataSearch, setDataSearch] = useState("");
+  const [isUpdate, setUpdate] = useState({
+    update: false,
+    data: "",
+  });
+
+  //GET DATA TODO FROM INPUT TODO
+  const inputTodo = (data) => {
+    list.push(data);
+  };
+
+  //INPUT TODO UPDATE
+  const inputTodoUpdate = (data) => {
+    const dataUpdate = list.map((item) => {
+      if (item.id === data.id) {
+        item.task = data.task;
+      }
+      return item;
+    });
+    list = dataUpdate;
+    setTodos(dataUpdate);
+    setUpdate({ update: false, data: "" });
+  };
+
+  // HANDLE CHECKED
+  const checkbox = (e) => {
+    const data = todos.map((item) => {
+      if (e.target.name === item.task) {
+        item.complete = !item.complete;
+      }
+      return item;
+    });
+    setTodos(data);
+  };
+
+  //HANDLE SEARCH
+  const search = () => {
+    const data = list.filter((item) => {
+      return dataSearch.toLocaleLowerCase() === item.task.toLocaleLowerCase();
+    });
+    setTodos(data);
+  };
+  //HANDLE BUTTON DONE'
+  const done = () => {
+    const data = list.filter((item) => {
+      return item.complete === true;
+    });
+    setTodos(data);
+  };
+
+  //HANDLE BUTTON TODO
+  const todoBtn = () => {
+    const data = list.filter((item) => {
+      return item.complete === false;
+    });
+    // list = data;
+    setTodos(data);
+  };
+
+  // HANDLE DELETE DONE TASK
+  const deleteAllDone = () => {
+    const data = list.filter((item) => {
+      return item.complete !== true;
+    });
+    list = data;
+    setTodos(data);
+  };
+
+  //HANDLE DELETE ALL
+  const deleteAll = () => {
+    setTodos("");
+    list = [];
+  };
+  //HANDLE DELETE
+  const delte = (name) => {
+    const data = list.filter((item) => {
+      return item.task !== name;
+    });
+    list = data;
+    setTodos(data);
+    setUpdate({
+      ...update,
+      update: false,
+    });
+  };
+  //HANDLE UPDATE
+  const update = (name) => {
+    const data = list.find((item) => {
+      return item.task === name;
+    });
+    setUpdate({
+      ...isUpdate,
+      update: true,
+      data: data,
+    });
+    setOpenInput(true);
+  };
+  console.log(isUpdate);
+  return (
+    <div className="todo_wrapper">
+      {openInput ? (
+        <Input
+          sub={setOpenInput}
+          funcInput={inputTodo}
+          update={isUpdate}
+          inputUpdate={inputTodoUpdate}
+        />
+      ) : (
+        <>
+          <h1>TodoSearch</h1>
+          <div className="todo__top">
+            <div className="top">
+              <div className="todo_search">
+                <div className="icon-box">
+                  <FaSearch className="icon" />
+                </div>
+                <input
+                  onChange={(e) => setDataSearch(e.target.value)}
+                  type="text"
+                  placeholder="Search Todo"
+                />
+              </div>
+              <button onClick={search} type="submit">
+                Search
+              </button>
+            </div>
+            <div className="bottom">
+              <button onClick={() => setOpenInput(true)} type="submit">
+                Add new Task
+              </button>
+            </div>
+          </div>
+          <h1 className="list_title">ToDoList</h1>
+          <div className="todo__botton">
+            <button onClick={() => setTodos(list)} type="button">
+              All
+            </button>
+            <button onClick={done} type="button">
+              Done
+            </button>
+            <button onClick={todoBtn} type="button">
+              ToDo
+            </button>
+          </div>
+          <div className="list_box">
+            {todos &&
+              todos.map((item) => (
+                <div key={item.id} className="todo_wrapp">
+                  <p>{item.task}</p>
+                  <div className="todo_listkanan">
+                    <input
+                      name={item.task}
+                      checked={item.complete}
+                      className="checkbox"
+                      onChange={checkbox}
+                      type="checkbox"
+                    />
+                    <FaPencilAlt
+                      onClick={() => update(item.task)}
+                      className="edit"
+                    />
+                    <MdDelete
+                      onClick={() => delte(item.task)}
+                      className="del"
+                    />
+                  </div>
+                </div>
+              ))}
+          </div>
+          <div className="dtn">
+            <button onClick={deleteAllDone}>Delete Done Tasks</button>
+            <button onClick={deleteAll}>Delete All Tasks</button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ToDo;
